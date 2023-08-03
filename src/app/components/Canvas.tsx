a731df83c27802d3b9c21dd318a5297b3d8ef5ba
stalliardo187 <darrenstallard27@hotmail.com>
2023-08-03 17:21:00 +0100

    Added player movement logic

--- src/app/components/Canvas.tsx
+++ src/app/components/Canvas.tsx
@@ -3,6 +3,17 @@ import React, { useRef, useEffect, useState } from 'react';
 
 const Canvas = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const keys = {
+    w: {
+      pressed: false
+    },
+    a: {
+      pressed: false
+    },
+    d: {
+      pressed: false
+    },
+  }
 
   useEffect(() => {
     if (!canvasRef.current) return;
@@ -27,29 +38,53 @@ const Canvas = () => {
       requestAnimationFrame(animate);
 
       const currentCtx = canvasRef.current?.getContext("2d");
-      if(!currentCtx) return;
+      if (!currentCtx) return;
 
       currentCtx.fillStyle = 'white';
       ctx?.fillRect(0, 0, cr.width, cr.height);
 
+      player.velocity.x = 0;
+
+      if (keys.d.pressed) player.velocity.x = 5; 
+      else if (keys.a.pressed) player.velocity.x = -5;
+      
       player.draw();
       player.update()
       // ctx?.clearRect(0, 0, canvasRef?.width ?? 0, canvasRef?.height ?? 0);
     }
 
     animate();
+
+    addEventListener("keydown", (event) => {
+      switch (event.key) {
+        case "w":
+          if (player.velocity.y === 0) player.velocity.y -= 20;
+          break;
+        case "a":
+          keys.a.pressed = true;
+          break;
+        case "d":
+          keys.d.pressed = true;
+          break;
+        default:
+          break;
+      }
+    })
+
+    addEventListener("keyup", (event) => {
+      switch (event.key) {
+        case "a":
+          keys.a.pressed = false;
+          break;
+        case "d":
+          keys.d.pressed = false;
+          break;
+        default:
+          break;
+      }
+    })
+
   }, [canvasRef]);
-
-
-
-
-
-
-
-
-
-
-
 
   return <div>
     <canvas ref={canvasRef}></canvas>
--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -19,12 +19,13 @@ export default class Player {
     }
 
     update() {
+        this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
+        this.sides.bottom = this.position.y + this.height;
 
         // Above bottom of canvas
         if (this.sides.bottom + this.velocity.y < this.canvas.height) {
             this.velocity.y += this.gravity;
-            this.sides.bottom = this.position.y + this.height;
         } else {
             this.velocity.y = 0;
         }
