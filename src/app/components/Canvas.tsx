9d405e3b1ada45b92da672fb275af5912708d6e5
stalliardo <46523228+stalliardo@users.noreply.github.com>
2023-08-04 13:37:44 +0100

    Added the Player class and some methods (#1)
    * Added the PLayer class and some methods
    
    * Added gravity to the player
    
    * Added player movement logic
    
    ---------
    
    Co-authored-by: stalliardo187 <darrenstallard27@hotmail.com>

--- src/app/components/Canvas.tsx
+++ src/app/components/Canvas.tsx
@@ -1,27 +1,93 @@
+import Player from '@/classes/Player';
 import React, { useRef, useEffect, useState } from 'react';
 
 const Canvas = () => {
-  const [canvasRef, setCanvasRef] = useState<HTMLCanvasElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const keys = {
+    w: {
+      pressed: false
+    },
+    a: {
+      pressed: false
+    },
+    d: {
+      pressed: false
+    },
+  }
 
   useEffect(() => {
-    if (!canvasRef) return;
+    if (!canvasRef.current) return;
 
     // Get the canvas context for drawing
-    const ctx = canvasRef.getContext('2d');
+    const ctx = canvasRef.current.getContext('2d');
+
     if (!ctx) return;
 
-    // Set the canvas size
-    canvasRef.width = 1024; // Change this to your desired width
-    canvasRef.height = 576; // Change this to your desired height
+    const cr = canvasRef.current;
 
-    // Draw something on the canvas (e.g., a red rectangle)
+    // set the canvas width
+    cr.width = 1024;
+    cr.height = 576;
+
     ctx.fillStyle = 'red';
-    ctx.fillRect(50, 50, canvasRef.width, canvasRef.height); // Change the coordinates and size as per your requirement
+    ctx.fillRect(100, 100, 100, 100);
+
+    const player = new Player(ctx, cr);
+
+    function animate() {
+      requestAnimationFrame(animate);
+
+      const currentCtx = canvasRef.current?.getContext("2d");
+      if (!currentCtx) return;
+
+      currentCtx.fillStyle = 'white';
+      ctx?.fillRect(0, 0, cr.width, cr.height);
+
+      player.velocity.x = 0;
+
+      if (keys.d.pressed) player.velocity.x = 5; 
+      else if (keys.a.pressed) player.velocity.x = -5;
+      
+      player.draw();
+      player.update()
+      // ctx?.clearRect(0, 0, canvasRef?.width ?? 0, canvasRef?.height ?? 0);
+    }
+
+    animate();
+
+    addEventListener("keydown", (event) => {
+      switch (event.key) {
+        case "w":
+          if (player.velocity.y === 0) player.velocity.y -= 20;
+          break;
+        case "a":
+          keys.a.pressed = true;
+          break;
+        case "d":
+          keys.d.pressed = true;
+          break;
+        default:
+          break;
+      }
+    })
+
+    addEventListener("keyup", (event) => {
+      switch (event.key) {
+        case "a":
+          keys.a.pressed = false;
+          break;
+        case "d":
+          keys.d.pressed = false;
+          break;
+        default:
+          break;
+      }
+    })
 
   }, [canvasRef]);
 
   return <div>
-    <canvas ref={setCanvasRef}></canvas>
+    <canvas ref={canvasRef}></canvas>
   </div>
 };
 
